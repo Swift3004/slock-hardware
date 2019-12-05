40353e2332c4f88e8101c884c225b1ddb3f4fef3
Swift3004 <0995412@hr.nl>
2019-12-05 17:07:18 +0100

    [ADD] Test dynamically add service

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,21 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-BLEServer* pServer = NULL;
+BLEServer* pServerMain = NULL;
 BLECharacteristic* pCharacteristic = NULL;
+BLEAdvertising *pAdvertising = NULL;
+ BLEService *pService2 = NULL;
 uint32_t deviceConnected = 0;
 uint32_t oldDeviceConnected = 0;
 uint32_t value = 0;
-
+uint32_t value2 = 0;
+uint32_t value3 = 0;
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 
 class MyServerCallbacks: public BLEServerCallbacks {
+
     void onConnect(BLEServer* pServer) {
       deviceConnected++;
       BLEDevice::startAdvertising();
@@ -23,6 +27,8 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected--;
+      value3 = 1;
+//BLEDevice::startAdvertising();
     }
 };
 
@@ -54,11 +60,11 @@ void setup() {
   BLEDevice::init("ESP32");
 
   // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  pServerMain = BLEDevice::createServer();
+  pServerMain->setCallbacks(new MyServerCallbacks());
 
   // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLEService *pService = pServerMain->createService(SERVICE_UUID);
 
   // Create a BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
@@ -78,10 +84,13 @@ void setup() {
   pService->start();
 
   // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+
+
+
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
 }
@@ -89,7 +98,7 @@ void setup() {
 void loop() {
     // notify changed value
     if (deviceConnected > 0) {
-        Serial.println(deviceConnected);
+        //Serial.println(deviceConnected);
         pCharacteristic->setValue((uint8_t*)&value, 4);
         pCharacteristic->notify();
         value++;
@@ -99,7 +108,7 @@ void loop() {
     if (!(deviceConnected > 0) && oldDeviceConnected > 0) {
        Serial.println("3");
         delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
+        pServerMain->startAdvertising(); // restart advertising
         Serial.println("start advertising");
         oldDeviceConnected = deviceConnected;
     }
@@ -107,5 +116,35 @@ void loop() {
     if (deviceConnected > 0 && !(oldDeviceConnected > 0)) {
         // do stuff here on connecting
         oldDeviceConnected = deviceConnected;
+    }
+
+    if (value2 == 0 && deviceConnected > 0) {
+      Serial.println("yo");
+       pService2 = pServerMain->createService("4fafc201-1fb5-459e-8fcc-c5c9c331915c");
+
+  // Create a BLE Characteristic
+  BLECharacteristic* pCharacteristic2 = pService2->createCharacteristic(
+                      "beb5483e-36e1-4688-b7f5-ea07361b26b9",
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+ 
+  
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic2->addDescriptor(new BLE2902());
+
+  // Start the service
+ // pService2->start();
+  BLEDevice::getAdvertising()->addServiceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331915c");
+  BLEDevice::startAdvertising();
+  value2 = 1;
+    }
+
+    if (value3 == 1) {
+      pService2->start();
+      value3 = 10;
     }
 }
