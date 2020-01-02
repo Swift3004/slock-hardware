5549fa4e47380ef340405dd19ffc151e0a6e5e4b
Swift3004 <0995412@hr.nl>
2020-01-02 14:30:27 +0100

    [ADD] Previous deleted code

--- src/serviceCallbacks.cpp
+++ src/serviceCallbacks.cpp
@@ -37,14 +37,14 @@ void ServiceCallbacks::onWrite(BLECharacteristic *pCharacteristic)
   }
   else if (pCharacteristic->getUUID().toString() == CHARACTERISTIC_UUID_AUTH_1)
   {
-    //  Serial.println(rxValue.c_str());
-    // std::string hash = rxValue.substr(0, 64);
-    // std::string action = rxValue.substr(65, 1);
+    Serial.println(rxValue.c_str());
+    std::string hash = rxValue.substr(0, 64);
+    std::string action = rxValue.substr(65, 1);
    
-    // bool response = authenticate(hash.c_str());
-    // if (response) {
-    //   // Open Lock
-    // }
+    bool response = authenticate(hash.c_str());
+    if (response) {
+      // Open Lock
+    }
   }
 }
 
